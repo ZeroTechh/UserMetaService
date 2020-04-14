01c947395b9139ce42898067b6551c82f18d49b7
Roshan Mehta <sonicroshan122@gmail.com>
2020-04-14 22:05:30 +0530

    Added main.go

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.14
 
 require (
 	github.com/ZeroTechh/UserExtraService v0.0.0-20200413111658-7490e0dcbd27
+	github.com/ZeroTechh/UserMainService v0.0.3 // indirect
 	github.com/ZeroTechh/VelocityCore v0.0.22
 	github.com/ZeroTechh/hades v0.1.9
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/stretchr/testify v1.5.1
 	go.mongodb.org/mongo-driver v1.3.2
+	go.uber.org/zap v1.14.1
 )
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/ZeroTechh/VelocityCore/logger"
+	proto "github.com/ZeroTechh/VelocityCore/proto/UserMetaService"
+	"github.com/ZeroTechh/VelocityCore/services"
+	"github.com/ZeroTechh/VelocityCore/utils"
+	"github.com/ZeroTechh/hades"
+
+	"github.com/ZeroTechh/UserMetaService/serviceHandler"
+)
+
+var (
+	config = hades.GetConfig("main.yaml", []string{"config"})
+	log    = logger.GetLogger(
+		config.Map("service").Str("logFile"),
+		config.Map("service").Bool("debug"),
+	)
+)
+
+func main() {
+	defer utils.HandlePanic(log)
+	defer log.Info("Service Stopped")
+
+	grpcServer, listner := utils.CreateGRPCServer(
+		services.UserMainService,
+		log,
+	)
+
+	serviceHandler := serviceHandler.New()
+
+	proto.RegisterUserMetaServer(grpcServer, serviceHandler)
+
+	log.Info("Service Started")
+	if err := grpcServer.Serve(*listner); err != nil {
+		log.Fatal("Service Failed With Error", zap.Error(err))
+	}
+}
